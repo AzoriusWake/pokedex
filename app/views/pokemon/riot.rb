6a44e0672eb26225dba5b1528cd5c03233945e6d
Henry Tam <IamHenry@Henrys-MacBook-Air.local>
2015-02-19 16:35:02 -0800

    added wrong file

--- app/views/pokemon/riot.rb
+++
@@ -1,7 +0,0 @@
-require 'pp'
-require 'httparty'
-
-url = "https://na.api.pvp.net/api/lol/na/v2.2/match/1688394830?api_key=4f143121-4b49-4dcd-a1b3-ef02c2dc51e7"
-
-response = HTTParty.get(url)
-pp response
